2ab661d770fda57c925a21bb7734ba0961380cfc
iluKane <139994343+iluKane@users.noreply.github.com>
2024-06-22 23:57:58 +0800

    Add files via upload

--- assets/js/kmain.js
+++ assets/js/kmain.js
@@ -1,55 +1,192 @@
-// 包含要添加到DOM中的各种元素的JSON数据
 var jsonData = [
-    { "tag": "h1", "text": "Lorem" },
-    { "tag": "p", "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae et" },
+    { tag: "h1", title: "CSS Shadow Box" },
     {
-        "tag": "code",
-        "class": "language-css",
-        "code": [
-            { "indent": "0", "value": "* {" },
-            { "indent": "1", "value": "margin: 0;" },
-            { "indent": "1", "value": "padding: 0;" },
-            { "indent": "1", "value": "box-sizing: border-box;" },
-            { "indent": "1", "value": "-webkit-user-select: none;" },
-            { "indent": "1", "value": "user-select: none;" },
-            { "indent": "0", "value": "}" }
-        ]
-    }
+        tag: "p",
+        text: "下面是一个简单示例",
+    },
+    {
+        tag: "div",
+        showHTML: false,
+        showCSS: true,
+        showJS: false,
+        code: {
+            html: [{ indent: "0", value: '<div class="box"></div>' }],
+            css: [
+                { indent: "0", value: ".box {" },
+                { indent: "1", value: "width: 100px;" },
+                { indent: "1", value: "height: 100px;" },
+                { indent: "1", value: "border: 1px solid #000;" },
+                { indent: "1", value: "box-shadow: 0px 0px 10px;" },
+                { indent: "0", value: "}" },
+            ],
+            js: [{ indent: "0", value: "console.log('JS代码运行中');" }],
+        },
+    },
+    { tag: "p", text: "测试1" },
+    { tag: "p", text: "测试2" },
 ];
 
-// 获取要将内容追加到的容器元素
+
 var container = document.getElementById("kmain");
 
-// 遍历jsonData中的每个项目
 jsonData.forEach(function (item) {
     createElements(item, container);
 });
 
 function createElements(data, parent) {
-    // 根据数据中指定的标签创建主元素
     var element = document.createElement(data.tag);
 
-    // 如果提供了文本内容，则设置文本内容
+    if (data.title) {
+        element.innerText = data.title;
+    }
     if (data.text) {
         element.innerText = data.text;
     }
 
-    // 如果存在代码块，则进行处理
     if (data.code) {
-        var preElement = document.createElement("pre");
+        var openBtns = document.createElement("div");
+        openBtns.className = "openBtns";
+        var codeBoxs = document.createElement("div");
+        codeBoxs.className = "codeBoxs";
 
-        var codeElement = document.createElement("code");
-        codeElement.className = data.class;
+        var openHTML = document.createElement("div");
+        openHTML.innerText = "HTML";
+        var openCSS = document.createElement("div");
+        openCSS.innerText = "CSS";
+        var openJS = document.createElement("div");
+        openJS.innerText = "JS";
+        var openIframe = document.createElement("div");
+        openIframe.innerText = "预览";
+        openIframe.className = "openIframe";
 
-        // 使用适当的缩进创建代码内容
-        codeElement.innerHTML = data.code.map(function (line) {
-            return " ".repeat(line.indent * 2) + line.value;
-        }).join("\n");
+        var htmlBox = document.createElement("div");
+        var cssBox = document.createElement("div");
+        var jsBox = document.createElement("div");
+        htmlBox.className = "htmlBox";
+        cssBox.className = "cssBox";
+        jsBox.className = "jsBox";
 
-        preElement.appendChild(codeElement);
-        element.appendChild(preElement);
+        openHTML.addEventListener("click", function () {
+            if (htmlBox.style.display == "none") {
+                htmlBox.style.display = "block";
+            } else {
+                htmlBox.style.display = "none";
+            }
+        });
+        openCSS.addEventListener("click", function () {
+            if (cssBox.style.display == "none") {
+                cssBox.style.display = "block";
+            } else {
+                cssBox.style.display = "none";
+            }
+        });
+        openJS.addEventListener("click", function () {
+            if (jsBox.style.display == "none") {
+                jsBox.style.display = "block";
+            } else {
+                jsBox.style.display = "none";
+            }
+        });
+
+        var iframeBox = document.createElement("iframe");
+        iframeBox.className = "iframeBox";
+
+        openBtns.append(openHTML, openCSS, openJS, openIframe);
+        codeBoxs.append(htmlBox, cssBox, jsBox, iframeBox);
+
+        element.appendChild(openBtns);
+        element.appendChild(codeBoxs);
+        element.className = "kcode";
+
+        parent.appendChild(element);
+        var htmlEditor = CodeMirror(htmlBox, {
+            value: data.code.html
+                .map((line) => " ".repeat(line.indent) + line.value)
+                .join("\n"),
+            mode: "htmlmixed",
+        });
+        var cssEditor = CodeMirror(cssBox, {
+            value: data.code.css
+                .map((line) => " ".repeat(line.indent) + line.value)
+                .join("\n"),
+            mode: "css",
+        });
+        var jsEditor = CodeMirror(jsBox, {
+            value: data.code.js
+                .map((line) => " ".repeat(line.indent) + line.value)
+                .join("\n"),
+            mode: "javascript",
+        });
+
+        if (data.showHTML) {
+            htmlBox.style.display = "block";
+        } else {
+            htmlBox.style.display = "none";
+        }
+        if (data.showCSS) {
+            cssBox.style.display = "block";
+        } else {
+            cssBox.style.display = "none";
+        }
+        if (data.showJS) {
+            jsBox.style.display = "block";
+        } else {
+            jsBox.style.display = "none";
+        }
+
+        // 初始加载时更新 iframe
+        updateIframe(
+            htmlEditor.getValue(),
+            cssEditor.getValue(),
+            jsEditor.getValue(),
+            iframeBox
+        );
+
+        openIframe.addEventListener("click", function () {
+            updateIframe(
+                htmlEditor.getValue(),
+                cssEditor.getValue(),
+                jsEditor.getValue(),
+                iframeBox
+            );
+        });
+    } else {
+        parent.appendChild(element);
     }
-
-    // 将创建的元素追加到父元素中
-    parent.appendChild(element);
 }
+
+function updateIframe(htmlContent, cssContent, jsContent, iframeBox) {
+    var iframeContent =
+        `<!DOCTYPE html>
+        <html lang="zh">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <style>${cssContent}</style>
+            </head>
+            <body>
+                ${htmlContent}
+                <script>
+                    function sendHeightToParent() {
+                        const height = document.documentElement.scrollHeight;
+                        window.parent.postMessage(height, '*');
+                    }
+                    window.onload = sendHeightToParent;
+                    window.onresize = sendHeightToParent;
+                    ${jsContent}
+                <\/script>
+            </body>
+        </html>`;
+    iframeBox.srcdoc = iframeContent;
+}
+
+// Add this listener in the parent page to adjust iframe height
+window.addEventListener('message', function (event) {
+    if (event.origin !== window.location.origin) {
+        return;
+    }
+    const iframes = document.querySelectorAll('iframe.iframeBox');
+    iframes.forEach(iframe => {
+        iframe.style.height = event.data + 'px';
+    });
+}, false);
